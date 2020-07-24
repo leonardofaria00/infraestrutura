20ddc2e445069ed2f6dcbedca9e1facdb2b3e469
LEONARDO FARIA DOS SANTOS <leonardofaria00@gmail.com>
2020-07-24 17:58:38 -0300

    [FIX] Install Wine for run Windows

--- setup.sh
+++ setup.sh
@@ -195,6 +195,15 @@ getMySQL() {
     # quit;
 }
 
+getWine(){
+    sudo dpkg --add-architecture i386 
+    wget -O - https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
+    sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ focal main'
+    sudo apt update
+    sudo apt -y install --install-recommends winehq-stable
+    winecfg
+}
+
 # Limpando caches e reiniciando a máquina
 
 restart() {
@@ -216,5 +225,6 @@ getDocker
 getDockerComposer
 # getComposer
 # getMySQL
+getWine
 getClean
 restart
